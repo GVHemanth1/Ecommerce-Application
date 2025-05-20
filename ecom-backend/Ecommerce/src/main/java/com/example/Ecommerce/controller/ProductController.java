48cb921c82ce19d62ec4b43228abc34e3ee197ec
GVHemanth <venkatahemanth135@gmail.com>
2025-05-20 22:24:38 +0530

    Initial commit

---
+++ ecom-backend/Ecommerce/src/main/java/com/example/Ecommerce/controller/ProductController.java
@@ -0,0 +1,62 @@
+package com.example.Ecommerce.controller;
+
+
+import com.example.Ecommerce.model.Product;
+import com.example.Ecommerce.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api")
+public class ProductController {
+
+    @Autowired
+    ProductService productService;
+
+    @GetMapping("/products")
+    public ResponseEntity<List<Product>> getProducts(){
+
+
+        return new ResponseEntity<>(productService.getAllProducts(), HttpStatus.OK);
+    }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable int id ){
+
+        Product product = productService.getProductById(id);
+
+        if(product.getId() >0)
+            return new ResponseEntity<>(product,HttpStatus.OK);
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+
+    }
+
+    @GetMapping("/product/{productId}/image")
+    public ResponseEntity<byte[]> getImageByProductId(@PathVariable int productId){
+        Product product = productService.getProductById(productId);
+        return new ResponseEntity<>(product.getImageData(), HttpStatus.OK);
+    }
+
+    @PostMapping("/product")
+    public ResponseEntity<?> addProduct(@RequestPart Product product, @RequestPart MultipartFile imageFile){
+
+        Product savedProduct = null;
+        try {
+            savedProduct = productService.addProduct(product, imageFile);
+            return new ResponseEntity<>(savedProduct, HttpStatus.CREATED);
+        } catch (IOException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+
+    }
+}
---
+++ ecom-backend/Ecommerce/src/main/java/com/example/Ecommerce/service/ProductService.java
@@ -0,0 +1,36 @@
+package com.example.Ecommerce.service;
+
+import com.example.Ecommerce.model.Product;
+import com.example.Ecommerce.repo.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    ProductRepo productRepo;
+
+    public List<Product> getAllProducts() {
+
+        return productRepo.findAll();
+    }
+
+    public Product getProductById(int id) {
+
+        return productRepo.findById(id).orElse(new Product(-1));
+    }
+
+    public Product addProduct(Product product, MultipartFile imageFile) throws IOException {
+
+        product.setImageName(imageFile.getName());
+        product.setImageType(imageFile.getContentType());
+        product.setImageData(imageFile.getBytes());
+
+        return productRepo.save(product);
+    }
+}
